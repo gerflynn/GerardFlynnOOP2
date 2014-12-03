4d291e1bd6917ce9d12c59c6753a9a77c11bc360
Gerard Flynn <gerard.flynn@hotmail.com>
2014-12-03 18:24:55 +0000

    Account class gets more methods and attributes to hold and increment progress of the account/stats

--- Account.java
+++ Account.java
@@ -1,23 +1,27 @@
-/**/
-
-import javax.swing.*;
+//Account.java
+/*This is an instantiable class.
+ *It provides methods to create an account and manage the balance*/
 
 public class Account{
-	//attributes
+	//Attributes
 	private String username;
-	private String password;	//char[]	//private Race score;
+	private String password;
 	private double balance;
+	private int wins;
+	private int races;
 	
 	public Account(){
-		this("user","p",0);
+		this("user","p",0,0,0);
 	}
-	public Account(String username, String password, double balance){
+	public Account(String username, String password, double balance, int wins, int losses){
 		this.username = username;
 		this.password = password;
 		this.balance = balance;
+		this.wins = wins;
+		this.races = races;
 	}
 	
-	//accessor methods
+	//Accessor Methods
 	public String getUsername(){
 		return username;
 	}
@@ -27,8 +31,14 @@ public class Account{
 	public double getBalance(){
 		return balance;
 	}
+	public int getWins(){
+		return wins;
+	}
+	public int getTotalRaces(){
+		return races;
+	}
 	
-	//mutator methods
+	//Mutator Methods
 	public void setUsername(String username){
 		this.username = username;
 	}
@@ -38,23 +48,37 @@ public class Account{
 	public void setBalance(double balance){
 		this.balance = balance;
 	}
+	public void setWins(int wins){
+		this.wins = wins;
+	}
+	public void setTotalRaces(int races){
+		this.races = races;
+	}
 	
-	//add balance method
+	//Add Balance Method
 	public void updateBalance(double lodgement){
 		balance += lodgement;
 	}
 	
-	//decriment stake
+	//Decriment Stake
 	public void decrimentStake(int stake){
 		//validation here?
 		balance -= stake;
 	}
-	//increment winnings
+	//Increment Winnings
 	public void incrementWinnings(int winnings){
 		balance += winnings;
 	}
+	//Increment Total Wins
+	public void incrementTotalWins(int winnings){
+		wins += winnings;
+	}
+	//Increment Total Losses
+	public void incrementTotalRaces(int betCount){
+		races += betCount*3;
+	}
 	
-	//return details toString method
+	//Return balance details toString Method
 	public String toString(){
 		return "Current balance is " + String.format("€%.2f",balance);
 	}
